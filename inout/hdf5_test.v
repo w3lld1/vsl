fc78be82df5af4847753f47b2dc10b0bcb78a9e1
w3lld1 <42353747+w3lld1@users.noreply.github.com>
2026-07-24 04:43:22 +0200

    test(inout): isolate HDF5 round-trip resources

--- inout/hdf5_test.v
+++ inout/hdf5_test.v
@@ -1,10 +1,8 @@
 module inout
 
 import os
+import rand
 import vsl.inout.h5
-
-const hdf5_test_dir = os.join_path(os.vtmp_dir(), 'vsl', 'inout')
-const hdf5_test_file = os.join_path(hdf5_test_dir, 'roundtrip.h5')
 
 fn test_hdf5_dataset_round_trip() ! {
 	if !os.exists_in_system_path('h5dump') {
@@ -12,20 +10,36 @@ fn test_hdf5_dataset_round_trip() ! {
 		return
 	}
 
-	os.mkdir_all(hdf5_test_dir)!
+	test_dir := os.join_path(os.vtmp_dir(), 'vsl-inout-${rand.uuid_v4()}')
+	test_file := os.join_path(test_dir, 'roundtrip.h5')
+	os.mkdir_all(test_dir)!
 	defer {
-		os.rmdir_all(hdf5_test_dir) or {}
+		os.rmdir_all(test_dir) or {}
 	}
 
 	expected := [f64(1.25), -2.5, 3.75]
-	file := h5.Hdf5File.new(hdf5_test_file)!
+	file := h5.Hdf5File.new(test_file)!
+	mut file_closed := false
+	defer {
+		if !file_closed {
+			file.close()
+		}
+	}
 	assert file.write_dataset1d('values', expected)! >= 0
 	file.close()
+	file_closed = true
 
 	mut actual := []f64{len: 1}
-	read_file := h5.open_file(hdf5_test_file)!
+	read_file := h5.open_file(test_file)!
+	mut read_file_closed := false
+	defer {
+		if !read_file_closed {
+			read_file.close()
+		}
+	}
 	read_file.read_dataset1d('values', mut actual)
 	read_file.close()
+	read_file_closed = true
 
 	assert actual == expected
 }
